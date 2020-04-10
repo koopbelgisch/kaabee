a09e558dccf6a7a561f8cc7d56597b3f01929aa5
Arne Gevaert <arnegevaert@gmail.com>
2020-04-10 20:29:32 +0200

    basic routes for stores

--- src/controllers/winkels.ts
+++ src/controllers/winkels.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
+import { getManager, getRepository } from "typeorm";
+import {Store} from "../models/store";
 
 /**
  * GET /winkels
  * Shows all stores
  */
 export async function getStores(req: Request, res: Response): Promise<void> {
-  res.render("stores");
+  const stores = await getManager().find(Store);
+  res.render("stores", {stores: stores});
 }
 
 /**
@@ -13,9 +16,8 @@ export async function getStores(req: Request, res: Response): Promise<void> {
  * Shows page for store with given id
  */
 export async function getStore(req: Request, res: Response): Promise<void> {
-  res.render("store", {
-    name: req.params["storeId"]
-  });
+  const store = await getRepository(Store).findOne(req.params["storeId"])
+  res.render("store", {store: store});
 }
 
 /**
