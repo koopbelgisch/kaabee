dd9ae6504b3c35dccca77a672f4d1ebabb0568ba
Rien Maertens <rien.maertens@posteo.be>
2020-05-23 22:58:02 +0200

    Proper tests for Tag

---
+++ src/test/store.test.ts
@@ -0,0 +1,37 @@
+import { launch, TestInstance } from "./helper";
+import { factory } from "../factory";
+
+let t: TestInstance;
+beforeAll(async () => {
+  t = await launch();
+});
+
+afterAll(async () => {
+  await t.server.close();
+});
+
+test("create valid store", async () => {
+  const store = await factory.store.create();
+  expect(store.id).toBeDefined();
+})
+
+test("get store index", async () => {
+  const stores = await factory.store.createAmount(10);
+  const resp = await t.app.get("/winkels");
+  expect(resp.status).toBe(200);
+  for(const store of stores) {
+    expect(resp.data).toContain(store.name);
+  }
+});
+
+test("get store show", async () => {
+  const store = await factory.store.create();
+  const resp = await t.app.get(`/winkels/${ store.id }`);
+  expect(resp.status).toBe(200);
+  expect(resp.data).toContain(store.description);
+});
+
+test("should get 404", async () => {
+  const resp = await t.app.get("/winkels/999999");
+  expect(resp.status).toBe(404);
+});
--- src/test/tag.test.ts
+++ src/test/tag.test.ts
@@ -1,4 +1,5 @@
 import { launch, TestInstance } from "./helper";
+import { factory } from "../factory";
 
 let t: TestInstance;
 beforeAll(async () => {
@@ -9,13 +10,28 @@ afterAll(async () => {
   await t.server.close();
 });
 
+test("create valid tags", async () => {
+  const tag = await factory.tag.create();
+  expect(tag.id).toBeDefined();
+});
 
 test("get tag index", async () => {
+  const tags = await factory.tag.createAmount(10);
   const resp = await t.app.get("/tags");
   expect(resp.status).toBe(200);
+  for (const tag of tags) {
+    expect(resp.data).toContain(tag.name);
+  }
 });
 
 test("get tag show", async () => {
-  const resp = await t.app.get("/tag/1");
+  const tag = await factory.tag.create();
+  const resp = await t.app.get(`/tags/${ tag.id }`);
   expect(resp.status).toBe(200);
+  expect(resp.data).toContain(tag.name);
+});
+
+test("should get 404", async () => {
+  const resp = await t.app.get("/tag/9999999");
+  expect(resp.status).toBe(404);
 });
