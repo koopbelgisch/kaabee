39f76647b266b00a7c701b327a7f2571ac75df2e
Rien Maertens <rien.maertens@posteo.be>
2020-06-04 22:58:47 +0200

    Fix ormconfig for testing and development

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,23 @@
 const config = require("config");
 
+let dir, ext;
+if (process.env.NODE_ENV === "test" || process[Symbol.for("ts-node.register.instance")]) {
+  // We're running in ts-node or in jest, provide ts sources.
+  dir = "src";
+  ext = "ts";
+} else {
+  // Provide compiles sources.
+  dir = "dist";
+  ext = "js";
+}
+
 module.exports = {
   "type": "sqlite",
   "database": config.get("database.location"),
   "synchronize": config.get("database.synchronize"),
   "logging": config.get("database.logging"),
   "entities": [
-    "dist/models/**/*.js"
-  ],
-  "migrations": [
-    "db/migrations/**/*.ts"
+    `${ dir }/models/**/*.${ ext }`
   ],
   "cli": {
     "entitiesDir": "src/models",
